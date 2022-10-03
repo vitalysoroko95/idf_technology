c789e540874d5c1dc47f713b3d374f35f8c597be
DESKTOP-RE8LH2A\Shprgd <isephar@gmail.com>
2022-10-03 07:53:15 +0300

    feat: add preloader

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,15 +3,23 @@ import Breadcrumbs from './Breadcrumbs';
 
 import Footer from './Footer';
 import Header from './Header';
+import Spinner from 'react-bootstrap/Spinner';
+import { useSelector } from 'react-redux';
 
 const Layout = ({ children }) => {
+  const { isLoading } = useSelector((store) => store.schema);
   return (
     <div className='flex min-h-screen  w-full flex-col justify-between'>
       <Header />
-      <main className='flex flex-col bg-[#1e293a] justify-start  h-[calc(100vh-10rem)] py-4'>
-        <Breadcrumbs />
-        {children}
-      </main>
+      {isLoading ? (
+        <Spinner animation='border' variant='danger' />
+      ) : (
+        <main className='flex flex-col bg-[#1e293a] justify-start  h-[calc(100vh-10rem)] py-4'>
+          <Breadcrumbs />
+          {children}
+        </main>
+      )}
+
       <Footer />
     </div>
   );
